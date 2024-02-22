11e118a1f6310ab767ab1da5cce26d966a334257
Ilya <95875204+luiofff@users.noreply.github.com>
2024-02-22 22:02:07 +0300

    Update quickSort.js

--- Recursion/quickSort.js
+++ Recursion/quickSort.js
@@ -1,9 +1,9 @@
-function qsort(arr) {
+const qsort = (arr) => {
     if (arr.length < 2) return arr;
 
     let pivot = arr[0];
-    let left = [];
-    let right = [];
+    let left = Array();
+    let right = Array();
 
     for (let i=1; i < arr.length; i++) (arr[i] < pivot) ? left.push(arr[i]) : right.push(arr[i]);
     return qsort(left).concat(pivot, qsort(right));
